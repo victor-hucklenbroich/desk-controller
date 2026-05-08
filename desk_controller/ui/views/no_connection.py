1749f8e592955e43da92291a8edee0a8358947c8
Victor Hucklenbroich <victor-h@outlook.de>
2026-05-08 23:18:33 +0200

    Add uuid field to connection error view

--- desk_controller/ui/views/no_connection.py
+++ desk_controller/ui/views/no_connection.py
@@ -11,6 +11,7 @@ from Foundation import NSObject, NSMakeRect
 
 import constants
 from constants import LOGGER
+from control import config
 from ui import window
 
 
@@ -37,20 +38,20 @@ class NoConnectionView(NSView):
         """Initializes and positions all UI elements within the popover."""
         # Error label
         error_label = NSTextField.alloc().initWithFrame_(
-            NSMakeRect(38, 40, 295, 50)
+            NSMakeRect(38, 78, 295, 30)
         )
         error_label.setStringValue_("⚠ Could not connect to your Desk!")
         error_label.setBezeled_(False)
         error_label.setDrawsBackground_(False)
         error_label.setEditable_(False)
         error_label.setSelectable_(False)
-        error_label.setTextColor_(NSColor.colorWithCalibratedWhite_alpha_(1, 0.6))
-        error_label.setFont_(NSFont.systemFontOfSize_(15))
+        error_label.setTextColor_(NSColor.colorWithCalibratedWhite_alpha_(1, 0.8))
+        error_label.setFont_(NSFont.systemFontOfSize_(14))
         error_label.setAlignment_(1)
         self.addSubview_(error_label)
 
         error_sub_label = NSTextField.alloc().initWithFrame_(
-            NSMakeRect(20, 17, 325, 50)
+            NSMakeRect(20, 57, 325, 30)
         )
         error_sub_label.setStringValue_("Please check your UUID and Bluetooth connection")
         error_sub_label.setBezeled_(False)
@@ -61,6 +62,21 @@ class NoConnectionView(NSView):
         error_sub_label.setFont_(NSFont.systemFontOfSize_(13))
         error_sub_label.setAlignment_(1)
         self.addSubview_(error_sub_label)
+
+        # UUID field
+        self.uuid_field = NSTextField.alloc().initWithFrame_(
+            NSMakeRect(12, 38, 338, 25)
+        )
+        self.uuid_field.setPlaceholderString_("Please enter the UUID of your desk and try again")
+        self.uuid_field.setStringValue_(constants.CONFIG_UUID)
+        self.uuid_field.setBezeled_(True)
+        self.uuid_field.setDrawsBackground_(True)
+        self.uuid_field.setEditable_(True)
+        self.uuid_field.setSelectable_(True)
+        self.uuid_field.setTextColor_(NSColor.whiteColor())
+        self.uuid_field.setFont_(NSFont.systemFontOfSize_(12))
+        self.uuid_field.setAlignment_(0)
+        self.addSubview_(self.uuid_field)
 
         # Version label
         version_label = NSTextField.alloc().initWithFrame_(
@@ -98,6 +114,10 @@ class NoConnectionView(NSView):
     def retry_(self, sender):
         """Triggers user initiated server retry"""
         LOGGER.debug("Retry button pressed")
+        uuid = self.uuid_field.stringValue()
+        if uuid != constants.CONFIG_UUID:
+            LOGGER.info(f"user updated UUID: {uuid}")
+            config.ConfigParser.update(uuid)
         self.app.server.retry()
         self.app.checkAndUpdatePopover()
 
