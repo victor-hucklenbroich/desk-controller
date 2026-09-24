0cbdd8c93ac0488284eae7d5ddf3fcb346d9fd0e
Victor Hucklenbroich <victor-h@outlook.de>
2026-09-24 16:13:12 +0200

    Increase to next release version

--- desk_controller/constants.py
+++ desk_controller/constants.py
@@ -35,7 +35,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 # --- Static Constants ---
-VERSION: str = "v1.3.3"
+VERSION: str = "v1.4.0"
 PLACEHOLDER_UUID: str = "AA:AA:AA:AA:AA:AA"
 
 # --- Desk height limits (cm, floats for mm precision) ---
